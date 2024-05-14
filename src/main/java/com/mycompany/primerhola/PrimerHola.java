2783b41f9c567ce37fc44a0ab1956d2cadc123d0
Usuario <Usuario@DESKTOP-974T09C>
2024-05-14 20:42:03 +0200

    prueba examen

---
+++ src/main/java/com/mycompany/primerhola/PrimerHola.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.primerhola;
+
+/**
+ *
+ * @author Usuario
+ */
+
+//comentario
+
+
+public class PrimerHola {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        
+        int numero = 4564636;
+        char letra = 'S';
+        String frase = "Hola hoy es lunes";
+        double decimal = 9.876;
+        boolean interruptor = true;
+        float numero_grande = 845327567;
+        short numero_pequeño = 324;
+        String[]vector_palabras;
+        int[]vector_numeros;
+        
+        numero = 92837;
+        
+        vector_numeros = new int[10];
+        for(int i=0; i < 10; i++){
+             vector_numeros[i] = i;
+        }
+        
+        for(int i=0; i < 10; i++){
+             vector_numeros[i] = (int)(Math.random()*10);
+             System.out.println(vector_numeros[i]);
+             System.out.println(); //salto de linea
+        }
+    }
+}
